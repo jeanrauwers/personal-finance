b2e6c7d770c7b207d9501401934cd0a002970d56
Jean Rauwers <jeanrauwers@gmail.com>
2020-01-13 14:10:57 +0000

    add list to transactions_list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,48 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List userTransactions;
+  final List<Transaction> transactions;
 
-  TransactionList(this.userTransactions);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                child: Text(
-                  '£${tx.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  child: Text(
+                    '£${transactions[index].amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
                   ),
-                  Text(DateFormat('dd-MM-yyyy').format(tx.date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ))
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Text(
+                        DateFormat('dd-MM-yyyy')
+                            .format(transactions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ))
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        // children: transactions.map((tx) {}).toList(),
+      ),
     );
   }
 }
