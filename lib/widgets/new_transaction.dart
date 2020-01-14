1d4a438bb8736490e4d277f3a0de9acdcad4967c
Jean Rauwers <jeanrauwers@gmail.com>
2020-01-14 19:17:32 +0000

    fix for submit button

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -47,7 +47,7 @@ class _NewTransactionState extends State<NewTransaction> {
             FlatButton(
               child: Text('Add Transaction'),
               onPressed: submitHandler,
-              textColor: Colors.purple,
+              textColor: Theme.of(context).primaryColor,
             )
           ],
         ),
