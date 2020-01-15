5da55e49d6ead79bedf13d8a4e164dc62ba499a5
Jean Rauwers <jeanrauwers@gmail.com>
2020-01-15 21:29:32 +0000

    Add Transactions chart

--- lib/main.dart
+++ lib/main.dart
@@ -44,12 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
   static DateTime now = DateTime.now();
 
   final List<Transaction> _userTransactions = [
-    // Transaction(id: 't01', title: 'New Jacket', amount: 128.33, date: now),
-    // Transaction(id: 't02', title: 'New Shoes', amount: 88, date: now),
-    // Transaction(id: 't03', title: 'Coffee', amount: 2.53, date: now),
-    // Transaction(id: 't04', title: 'Dinner', amount: 39.73, date: now),
-    // Transaction(id: 't05', title: 'Plane Ticket', amount: 480.25, date: now),
+    Transaction(id: 't01', title: 'New Jacket', amount: 128.33, date: now),
+    Transaction(id: 't02', title: 'New Shoes', amount: 88, date: now),
+    Transaction(id: 't03', title: 'Coffee', amount: 2.53, date: now),
+    Transaction(id: 't04', title: 'Dinner', amount: 39.73, date: now),
+    Transaction(id: 't05', title: 'Plane Ticket', amount: 480.25, date: now),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((elem) {
+      return elem.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTransaction = Transaction(
@@ -76,6 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print(_recentTransactions);
     return Scaffold(
         appBar: AppBar(
           title: Text('Personal Expenses'),
@@ -90,7 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Chart(),
+                Chart(
+                  recentTransactions: _recentTransactions,
+                ),
                 TransactionList(_userTransactions),
               ]),
         ),
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart({this.recentTransactions});
+
+  List<Map<String, Object>> get groupedTransactionsValues {
+    return List.generate(7, (i) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: i),
+      );
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {
+        'day': DateFormat.E().format(weekDay),
+        'amount': totalSum,
+      };
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Card(
-        color: Colors.blue,
-        child: Container(
-          child: Text(
-            'CHART',
-          ),
+    return Card(
+      color: Colors.blue,
+      child: Container(
+        child: Row(
+          children: <Widget>[Text('CHART')],
         ),
-        elevation: 5,
       ),
+      elevation: 5,
     );
   }
 }
